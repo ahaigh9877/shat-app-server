41c43024eb38fbc37ab713091cc230d73bde64a0
ahaigh9877 <alastairhaigh@me.com>
2019-11-15 09:59:38 +0100

    made it say FFFFFFUUUUUUCCCCCKKKK

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 4000;
 
-app.listen(() =>
+app.listen(port, () =>
   console.log(`SHAT-App listens all the time to the port called "${port}"`)
 );
+
+app.get("/", (req, res, next) => {
+  res.send("FFFFUUUUUCCCCCKKKKK");
+});
