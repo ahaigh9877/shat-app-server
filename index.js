daabd5db6d25d56d8f8bfe741800f9c79b9e2453
ahaigh9877 <alastairhaigh@me.com>
2019-11-15 10:26:10 +0100

    done a get messages route

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-const jsonParser = bodyParser();
-
 const port = 4000;
 
+// This is necessary to read the body of the request!!
+const jsonParser = bodyParser();
 app.use(jsonParser);
 
+// standin for a proper database. That's why it's defined at the top-level, in global scope.
+// re-created every time the server starts.
 const messages = [];
 
 app.get("/", (req, res, next) => {
   res.send("FFFFUUUUUCCCCCKKKKK");
 });
 
+// Post request, sending a message, storing it in the array and returning it as a response.
 app.post("/message", (req, res, next) => {
   const { message } = req.body;
   messages.push(message);
   res.send(message);
+});
+
+app.get("/message", (req, res, next) => {
+  res.send(messages);
 });
 
 app.listen(port, () =>
