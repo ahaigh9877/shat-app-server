95383700f559429306c8af487aa67e548404ef81
ahaigh9877 <alastairhaigh@me.com>
2019-11-15 10:38:27 +0100

    added streamy endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// capitalised cos it's a class.
+const Sse = require("json-sse");
 
 const app = express();
 const port = 4000;
@@ -14,6 +16,18 @@ const messages = [];
 
 app.get("/", (req, res, next) => {
   res.send("FFFFUUUUUCCCCCKKKKK");
+});
+
+// Essentially a list of clients to which all data is sent.
+const stream = new Sse();
+
+app.get("/stream", (req, res, next) => {
+  // serialise the data!!!
+  const string = JSON.stringify(messages);
+  // add it to the stream...
+  stream.updateInit(string);
+  // then, this function adds the client to the stream. Stands in for res.send()
+  stream.init(req, res);
 });
 
 // Post request, sending a message, storing it in the array and returning it as a response.
