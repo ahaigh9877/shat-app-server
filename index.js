c5c8915d00ad305e459fa5a352e9bbf75c8f040c
ahaigh9877 <alastairhaigh@me.com>
2019-11-15 10:05:59 +0100

    made a new message endpoint

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const jsonParser = bodyParser();
+
 const port = 4000;
 
-app.listen(port, () =>
-  console.log(`SHAT-App listens all the time to the port called "${port}"`)
-);
+app.use(jsonParser);
+
+const messages = [];
 
 app.get("/", (req, res, next) => {
   res.send("FFFFUUUUUCCCCCKKKKK");
 });
+
+app.post("/message", (req, res, next) => {
+  const { message } = req.body;
+  messages.push(message);
+  res.send(message);
+});
+
+app.listen(port, () =>
+  console.log(`SHAT-App listens all the time to the port called "${port}"`)
+);
